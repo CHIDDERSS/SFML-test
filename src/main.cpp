2d98e8e06d1b534e3c42fc79527aa29f2d92a687
chidders <186743421+CHIDDERSS@users.noreply.github.com>
2025-08-04 12:47:48 +0100

    blue circle

--- src/main.cpp
+++ src/main.cpp
@@ -3,38 +3,49 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 
-int main() {
+class Game {
+public:
+    Game(const Game&) = delete;
+    Game& operator=(const Game&) = delete;
+    Game();
+    void run();
+private:
+    void processEvents();
+    void update();
+    void render();
+
+    sf::RenderWindow _window;
+    sf::CircleShape _player;
+};
+
+int main(int argc, char* argv[]) {
     // Create a window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML TEST");
     sf::Font font;
     font.loadFromFile("/usr/share/fonts/truetype/msttcorefonts/Arial.ttf");
     
+    sf::CircleShape circle(150);
+    circle.setFillColor(sf::Color::Blue);
+    circle.setPosition(10, 20);
+
     sf::Text text;
     text.setPosition(100, 100);
 
-    sf::CircleShape shape(200);
+    sf::CircleShape shape(150);
 
     std::string userInput;
 
     while (window.isOpen()) {
-      sf::Event event;
-      while(window.pollEvent(event)) {
-        if (event.type == sf::Event::Closed) {
-            window.close();
-        } else if (event.type == sf::Event::TextEntered) {
-            if (event.text.unicode == '\b' && !userInput.empty()) {
-                userInput.pop_back();
-            } else if (event.text.unicode < 120 && event.text.unicode != '\r') {
-                userInput += static_cast<char>(event.text.unicode);
-            }
-            text.setString(userInput);
-        }
-      }
+        sf::Event event;
 
-      window.clear();
-      window.draw(text);
-      window.draw(shape);
-      window.display();
+        while (window.pollEvent(event)) {
+            if ((event.type == sf::Event::Closed) || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
+                window.close();
+        }
+
+        window.clear();
+        window.draw(circle);
+        window.display();
     }
 
     return 0;

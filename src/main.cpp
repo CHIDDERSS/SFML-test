9b9f89898ea05826a18cd04726c7fae512d22f20
chidders <186743421+CHIDDERSS@users.noreply.github.com>
2025-08-04 11:55:01 +0100

    draw circle

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,36 @@
 int main() {
     // Create a window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML TEST");
-    sf::CircleShape shape(200);
+    sf::Font font;
+    font.loadFromFile("/usr/share/fonts/truetype/msttcorefonts/Arial.ttf");
     
-    while (window.isOpen()) {
-        sf::Event event;
-        while(window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-            window.close();
-        }
+    sf::Text text;
+    text.setPosition(100, 100);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+    sf::CircleShape shape(200);
+
+    std::string userInput;
+
+    while (window.isOpen()) {
+      sf::Event event;
+      while(window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            window.close();
+        } else if (event.type == sf::Event::TextEntered) {
+            if (event.text.unicode == '\b' && !userInput.empty()) {
+                userInput.pop_back();
+            } else if (event.text.unicode < 120 && event.text.unicode != '\r') {
+                userInput += static_cast<char>(event.text.unicode);
+            }
+            text.setString(userInput);
+        }
+      }
+
+      window.clear();
+      window.draw(text);
+      window.draw(shape);
+      window.display();
     }
+
+    return 0;
 }

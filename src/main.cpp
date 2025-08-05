aa40896e65817ed48e27ba4d7e77108862414669
chidders <186743421+CHIDDERSS@users.noreply.github.com>
2025-08-05 15:29:52 +0100

    added more classes, constructors and methods

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 #include <cmath>
+#include <cstring>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -23,7 +24,7 @@ private:
     void render();
 
     sf::RenderWindow _window;
-    sf::CircleShape _player;
+    Player _player;
 };
 
 // -------------------------------------------------- PLAYER CLASS --------------------------------------------------
@@ -42,16 +43,44 @@ public:
     bool isMoving;
     int rotation;
 
+public:
+    void processEvents() {
+        isMoving = sf::Keyboard::isKeyPressed(sf::Keyboard::Up);
+        rotation = 0;
+        rotation -= sf::Keyboard::isKeyPressed(sf::Keyboard::Left);
+        rotation += sf::Keyboard::isKeyPressed(sf::Keyboard::Right);
+    }
+
 private:
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
     sf::RectangleShape _shape;
     sf::Vector2f _velocity;
 };
 
+// -------------------------------------------------- ACTION CLASS --------------------------------------------------
+class Action {
+public:
+    enum Type {
+        RealTime = 1,
+        Pressed = 1 << 1,
+        Released = 1 << 2
+    };
+    Action (const sf::Keyboard::Key& key, int type = Type::RealTime|Type::Pressed);
+    Action (const sf::Mouse::Button& button, int type = Type::RealTime|Type::Pressed);
+
+    bool test() const;
+    bool operator==(const sf::Event& event) const;
+    bool operator==(const Action& other) const;
+
+private:
+    friend class ActionTarget;
+    sf::Event _event;
+    int _type;
+};
+
 // -------------------------------------------------- GAME CONSTRUCTOR --------------------------------------------------
 // constructor
-Game::Game() : _window(sf::VideoMode(800,600), "GAME"), _player(150) {
-    _player.setFillColor(sf::Color::Blue);
+Game::Game() : _window(sf::VideoMode(800,600), "GAME"), _player() {
     _player.setPosition(10,20);
 }
 
@@ -60,9 +89,14 @@ void Game::processEvents() {
     sf::Event event;    
     
     while(_window.pollEvent(event)) {
-        if ((event.type == sf::Event::Closed) || ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))) {
+        if (event.type == sf::Event::Closed) {
             _window.close();
-        }
+       } else if (event.type == sf::Event::KeyPressed) {
+            if (event.key.code == sf::Keyboard::Escape) {
+                _window.close();
+            }
+       } 
+       _player.processEvents(); 
     }
 }
 
@@ -108,7 +142,7 @@ Player::Player() : _shape(sf::Vector2f(32, 32)) {
 void Player::update(sf::Time deltaTime) {
     float seconds = deltaTime.asSeconds();
     if (rotation != 0) {
-        float angle = (rotation>0?1:-1)*180*seconds;
+        float angle = rotation*180*seconds;
         _shape.rotate(angle);
     }
     if(isMoving) {
@@ -116,6 +150,23 @@ void Player::update(sf::Time deltaTime) {
         _velocity += sf::Vector2(std::cos(angle), std::sin(angle)) * 60.0f * seconds;
     }
     _shape.move(seconds * _velocity);
+}
+
+void Player::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    target.draw(_shape, states);
+}
+
+// -------------------------------------------------- ACTION METHODS --------------------------------------------------
+
+/* copy action to another instance of action */
+Action::Action(const Action& other) : _type(other._type) {
+    std::memcpy(&_event, &other._event, sizeof(sf::Event));
+}
+
+Action& Action::operator=(const Action& other) {
+    std::memcpy(&_event, &other._event, sizeof(sf::Event));
+    _type = other._type;
+    return *this;
 }
 
 // -------------------------------------------------- MAIN --------------------------------------------------

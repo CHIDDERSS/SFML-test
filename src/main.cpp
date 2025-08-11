0a379bcd456260e74aa947144fd7227f2f8d2fc4
chidders <186743421+CHIDDERSS@users.noreply.github.com>
2025-08-11 14:22:31 +0100

    added processing Events in class etc

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <cstring>
+#include <list>
+#include <functional>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -11,8 +13,48 @@
 
 const int frames = 30;
 
+// -------------------------------------------------- ACTION CLASSES --------------------------------------------------
+class Action {
+public:
+    enum Type {
+        RealTime = 1,
+        Pressed = 1 << 1,
+        Released = 1 << 2
+    };
+    Action (const sf::Keyboard::Key& key, int type = Type::RealTime|Type::Pressed);
+    Action (const sf::Mouse::Button& button, int type = Type::RealTime|Type::Pressed);
+
+    Action(const Action& other); // <-- Add this line
+    Action& operator=(const Action& other); // <-- Add this line
+
+    bool test() const;
+    bool operator==(const sf::Event& event) const;
+    bool operator==(const Action& other) const;
+
+private:
+    friend class ActionTarget;
+    sf::Event _event;
+    int _type;
+};
+
+class ActionTarget {
+public:
+    using FuncType = std::function<void(const sf::Event&)>;
+    ActionTarget();
+
+    bool processEvent(const sf::Event& event) const;
+    void processEvents();
+
+    void bind(const Action& action, const FuncType& callback);
+    void unbind(const Action& action);
+
+private:
+    std::list<std::pair<Action, FuncType>> _eventRealTime;
+    std::list<std::pair<Action, FuncType>> _eventPoll;
+};
+
 // -------------------------------------------------- PLAYER CLASS --------------------------------------------------
-class Player  : public sf::Drawable {
+class Player  : public sf::Drawable, public ActionTarget {
 public:
     Player(const Player&) = delete;
     Player& operator = (const Player&) = delete;
@@ -24,15 +66,12 @@ public:
     }
 
     void update(sf::Time deltaTime);
-    bool isMoving;
-    int rotation;
+    bool _isMoving;
+    int _rotation;
 
 public:
     void processEvents() {
-        isMoving = sf::Keyboard::isKeyPressed(sf::Keyboard::Up);
-        rotation = 0;
-        rotation -= sf::Keyboard::isKeyPressed(sf::Keyboard::Left);
-        rotation += sf::Keyboard::isKeyPressed(sf::Keyboard::Right);
+        ActionTarget::processEvents();
     }
 
 private:
@@ -57,35 +96,10 @@ private:
     Player _player;
 };
 
-
-// -------------------------------------------------- ACTION CLASS --------------------------------------------------
-class Action {
-public:
-    enum Type {
-        RealTime = 1,
-        Pressed = 1 << 1,
-        Released = 1 << 2
-    };
-    Action (const sf::Keyboard::Key& key, int type = Type::RealTime|Type::Pressed);
-    Action (const sf::Mouse::Button& button, int type = Type::RealTime|Type::Pressed);
-
-    Action(const Action& other); // <-- Add this line
-    Action& operator=(const Action& other); // <-- Add this line
-
-    bool test() const;
-    bool operator==(const sf::Event& event) const;
-    bool operator==(const Action& other) const;
-
-private:
-    friend class ActionTarget;
-    sf::Event _event;
-    int _type;
-};
-
 // -------------------------------------------------- GAME CONSTRUCTOR --------------------------------------------------
 // constructor
 Game::Game() : _window(sf::VideoMode(800,600), "GAME"), _player() {
-    _player.setPosition(10,20);
+    _player.setPosition(150,150);
 }
 
 // -------------------------------------------------- GAME METHODS -------------------------------------------------- 
@@ -100,12 +114,12 @@ void Game::processEvents() {
                 _window.close();
             }
        } 
-       _player.processEvents(); 
     }
+    _player.processEvents(); 
 }
 
 void Game::update(sf::Time deltaTime) {
-
+    _player.update(deltaTime);
 };
 
 void Game::render() {
@@ -138,22 +152,38 @@ void Game::run(int frames_per_second) {
 }
 
 // -------------------------------------------------- PLAYER METHODS --------------------------------------------------
-Player::Player() : _shape(sf::Vector2f(32, 32)) {
+Player::Player() : _shape(sf::Vector2f(32, 32)), _isMoving(false), _rotation(0) {
     _shape.setFillColor(sf::Color::Blue);
     _shape.setOrigin(16,16);
+
+    bind(Action(sf::Keyboard::Up), [this](const sf::Event&) {
+        _isMoving = true;
+    });
+
+    bind(Action(sf::Keyboard::Left), [this](const sf::Event&) {
+        _rotation -= 1;
+    });
+
+    bind(Action(sf::Keyboard::Right), [this](const sf::Event&) {
+        _rotation += 1;
+    });
 }
 
 void Player::update(sf::Time deltaTime) {
     float seconds = deltaTime.asSeconds();
-    if (rotation != 0) {
-        float angle = rotation*180*seconds;
+    if (_rotation != 0) {
+        float angle = _rotation*180*seconds;
         _shape.rotate(angle);
     }
-    if(isMoving) {
+    if(_isMoving) {
         float angle = _shape.getRotation() / 180 * M_PI - M_PI / 2;
         _velocity += sf::Vector2(std::cos(angle), std::sin(angle)) * 60.0f * seconds;
     }
     _shape.move(seconds * _velocity);
+
+    // Reset flags at the end of the frame
+    _isMoving = false;
+    _rotation = 0;
 }
 
 void Player::draw(sf::RenderTarget& target, sf::RenderStates states) const {
@@ -161,7 +191,6 @@ void Player::draw(sf::RenderTarget& target, sf::RenderStates states) const {
 }
 
 // -------------------------------------------------- ACTION METHODS --------------------------------------------------
-
 /* copy action to another instance of action since no pointers in sf::Keyboard */
 Action::Action(const Action& other) : _type(other._type) {
     std::memcpy(&_event, &other._event, sizeof(sf::Event));
@@ -186,13 +215,97 @@ Action::Action(const sf::Mouse::Button& button, int type) : _type(type) {
 bool Action::operator==(const sf::Event& event) const {
     bool res = false;
     switch(event.type) {
-        case sf::Event::EventType::KeyPressed: 
+        case sf::Event::EventType::KeyPressed:
         {
-            if (_type& Type::Pressed && _event.type == sf::Event::EventType::KeyPressed)
-            res = event.key.code == _event.key.code;
-        } break;
+            if (_type & Type::Pressed && _event.type == sf::Event::EventType::KeyPressed)
+                res = event.key.code == _event.key.code;
+        }
+        break;
         case sf::Event::EventType::KeyReleased:
-            
+        {
+            if(_type & Type::Released && _event.type == sf::Event::EventType::KeyPressed) {
+                res = event.key.code == _event.key.code;
+            }
+        }
+        case sf::Event::EventType::MouseButtonPressed:
+        {
+            if(_type & Type::Pressed && _event.type == sf::Event::EventType::MouseButtonPressed) {
+                res = event.mouseButton.button == _event.mouseButton.button;
+                std::cout << "MOUSE BUTTON PRESSED! \n";
+            }
+        }
+        case sf::Event::EventType::MouseButtonReleased:
+        {
+            if(_type & Type::Released && _event.type == sf::Event::EventType::MouseButtonReleased) {
+                res = event.mouseButton.button == _event.mouseButton.button;
+                std::cout << "Mouse button released! \n";
+            }
+        } break;
+        default: break;
+    }
+    return res;
+}
+
+bool Action::operator==(const Action& other) const {
+    return _type == other._type && other == _event;
+}
+
+bool Action::test () const {
+    bool res = false;
+    if (_event.type == sf::Event::EventType::KeyPressed) {
+        if (_type & Type::Pressed) {
+            res = sf::Keyboard::isKeyPressed(_event.key.code);
+        } else if (_event.type == sf::Event::EventType::MouseButtonPressed) {
+            if (_type & Type::Pressed) {
+                res = sf::Mouse::isButtonPressed(_event.mouseButton.button);
+            }
+            return res;
+        }
+    }
+    return res;
+}
+
+ActionTarget::ActionTarget() {
+
+}
+
+bool ActionTarget::processEvent(const sf::Event& event) const {
+    bool res = false;
+    for (auto& i : _eventPoll){
+        if (i.first == event) {
+            i.second(event);
+            res = true;
+            break;
+        }
+    }
+    return res;
+}
+
+void ActionTarget::processEvents() {
+    for (auto& i : _eventRealTime) {
+        if(i.first.test()) {
+            i.second(i.first._event);
+        }
+    }
+}
+
+void ActionTarget::bind(const Action& action, const FuncType& callback) {
+    if(action._type & Action::Type::RealTime) {
+        _eventRealTime.emplace_back(action, callback);
+    } else {
+        _eventPoll.emplace_back(action, callback);
+    }
+}
+
+void ActionTarget::unbind(const Action& action) {
+    auto remove_func = [&action](const std::pair<Action, FuncType>& pair) -> bool {
+        return pair.first == action;
+    };
+
+    if (action._type& Action::Type::RealTime) {
+        _eventRealTime.remove_if(remove_func);
+    } else {
+        _eventPoll.remove_if(remove_func);
     }
 }
 
@@ -200,4 +313,4 @@ bool Action::operator==(const sf::Event& event) const {
 int main(int argc, char* argv[]) {
     Game game;
     game.run(frames); // Run the game at 60 frames per second
-}           
+}
